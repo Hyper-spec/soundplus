34c1bc83e91a10cc2642a9e1e2ca8b65d5063607
João Victor <67173325+Hyper-spec@users.noreply.github.com>
2022-08-14 21:31:49 -0300

    update

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ['storage.googleapis.com', 'open.spotify.com'],
+    }
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import format from 'date-fns/format';
+import enUS from 'date-fns/locale/en-US';
+import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
+import { useState } from 'react';
+import { useTheme } from '../../contexts/ThemeContextProvider';
+import styles from './styles.module.scss';
+
+export function Header() {
+	const currentDate = format(new Date(), 'HH', {
+		locale: enUS,
+	});
+	const { isDarkMode, switchTheme } = useTheme();
+
+	return (
+		<header id='appHeader' className={styles.headerContainer}>
+            <h1 className={currentDate >= '18' && currentDate <= '23' ? styles.night :  styles.noNight}>Good Night</h1>
+            <h1 className={currentDate >= '00' && currentDate <= '11' ? styles.morning :  styles.noMorning}>Good Morning</h1>
+            <h1 className={currentDate >= '12' && currentDate <= '17' ? styles.afternoon :  styles.noAfternoon}>Good Afternoon</h1>
+
+			<button
+				type='button'
+				onClick={() => switchTheme(!isDarkMode)}
+				className={styles.themeSwitcher}>
+				<img
+					src={isDarkMode ? '/sun-light.svg' : '/moon-dark.svg'}
+					alt='Switch Theme'
+					title='Switch Theme'
+				/>
+			</button>
+		</header>
+	);
+}
