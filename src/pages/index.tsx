bfb8675cd44a466348cded9ca511660abbe787bf
João Victor <67173325+Hyper-spec@users.noreply.github.com>
2022-09-10 20:34:21 -0300

    new

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -139,7 +139,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
 	const { data } = await api.get('episodes', {
 		params: {
-			_limit: 20,
+			_limit: 22,
 			_sort: 'published_at',
 			_order: 'desc',
 		},
